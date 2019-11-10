40472fa208cfc231f749e2ed9f83cdf15bd17f02
Kirill Prokudin <kir.prokudin@yandex.ru>
2019-11-10 19:34:18 +0700

    ADDS ARTICLES & BOOTSTRAP
    Adds main page with all articles, and also a page for displaying any single article. Adds bootstrap.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ArticleController@index')->name('home');
+Route::get('/articles/{id}', 'ArticleController@show')->name('article.show');
