17012d7323ee9946a2985a47dda94cd01b9eaa1d
kaetan <kir.prokudin@yandex.ru>
2020-06-29 08:19:52 +0700

    * fixes

--- app/Http/Controllers/ImagePkController.php
+++ app/Http/Controllers/ImagePkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use ImagePk\ImagePk;
 
 class ImagePkController extends Controller
@@ -9,11 +10,14 @@ class ImagePkController extends Controller
     public function applyWm()
     {
         $imagePath = base_path() . '/public/img/Lighthouse.jpg';
-        $watermarkPath = base_path() . '/public/img/watermark.jpeg';
+        $watermarkPath = base_path() . '/public/img/logo.png';
         $resultPath = base_path() . '/public/img/';
 
         $service = new ImagePk();
-        $service->applyWatermark($imagePath, $watermarkPath, $resultPath);
+        $img = $service->applyWatermark("https://s0.rbk.ru/v6_top_pics/media/img/0/50/755858203490500.jpeg", "https://s0.rbk.ru/v6_top_pics/media/img/0/50/755858203490500.jpeg", $resultPath);
+        $img = $service->applyWatermark($resultPath, "https://s0.rbk.ru/v6_top_pics/media/img/0/50/755858203490500.jpeg", $resultPath);
+        $img = $service->applyWatermark($resultPath, "https://s0.rbk.ru/v6_top_pics/media/img/0/50/755858203490500.jpeg", $resultPath);
+        $img = $service->applyWatermark($resultPath, "https://s0.rbk.ru/v6_top_pics/media/img/0/50/755858203490500.jpeg", $resultPath);
     }
 
 }
